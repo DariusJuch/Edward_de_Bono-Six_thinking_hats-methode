ac929b8bc44c69d636c46b33168b50574c9c729d
Darius <juchevicd@gmail.com>
2025-04-04 23:52:21 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=""> 
+        <h1 className="text-amber-500">Hello world</h1>
+        <button className="btn">Default</button>
+      </div>
+
+      <div>
+        <h1 className=" text-amber-600"> Hi</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
