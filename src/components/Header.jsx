d3f735a2cf226181fd5b793c8b7b7ee4b0db74a6
Darius <juchevicd@gmail.com>
2025-04-17 09:45:59 +0400

    Fix bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Hat from '../../public/top-hat.svg'
+ 
 const Header = () => {
   return (
     <header className="bg-white shadow-sm">
       <div className="max-w-7xl py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <div className="flex items-center">
-          <img src="/top-hat.svg" alt="Six Thinking Hats Logo" className="h-8 w-auto pl-8" />
+          <img src={Hat} alt="Six Thinking Hats Logo" className="h-8 w-auto pl-8" />
           <span className="ml-3 text-lg font-semibold text-gray-900">Six Thinking Hats</span>
         </div>
       </div>
